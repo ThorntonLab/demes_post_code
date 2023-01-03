338351fea3f29587da09eb7ecd9d67f9c65ddaf4
Kevin R. Thornton <krthornt@uci.edu>
2023-01-03 11:30:32 -0800

    use rose pine for demes colors

--- example_model.py
+++ example_model.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 
+plt.style.use("rose-pine-moon")
 import demes
 import demesdraw
 
+
 graph = demes.load("demes_post_model.yaml")
 
-tubes = demesdraw.tubes(graph)
+tubes = demesdraw.tubes(
+    graph, colours={"ancestor": "#3e8fb0", "derived1": "#ea9a97", "derived2": "#c4a7e7"}
+)
 
 plt.savefig("demes_post_model.png")
